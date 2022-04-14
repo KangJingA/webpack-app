da336284908b8899680805948e3e7474165ae96b
Nicholas Ang <nicholas.ang@data4life-asia.care>
2022-04-14 13:53:16 +0800

    refactor imports/exports for webpack to bundle

--- src/app/utils/inputs-are-valid.js
+++ src/app/utils/inputs-are-valid.js
@@ -1,3 +1,4 @@
-const inputsAreValid = (...input) => {
+// must export/import accordingly to be explicit with our dependencies
+export const inputsAreValid = (...input) => {
     return input.every(num => typeof num === "number" && !isNaN(num));
   };
