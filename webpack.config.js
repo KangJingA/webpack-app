68e946e3944f10f8f4688ce023e0dcd4ca5e5412
Nicholas Ang <nicholas.ang@data4life-asia.care>
2022-04-14 16:18:32 +0800

    cache busting and html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development", // production mode by default
   devtool: false, // remove eval plus other words so that its easier to read the built code
   entry: "./src/index.js", // explicit entrypoint for webpack
   output: {
-    filename: "main.js",
+    filename: "main.[contenthash].js", //content hashing to cache bust
     path: path.resolve(__dirname, "dist"), // __dirname in a node script returns the path of the folder where the current JavaScript file resides.
   },
+  plugins: [ // plugins customize the webpack build process in a variety of ways
+    new HtmlWebpackPlugin({
+      template: "./src/template.html", //use a template html so that webpack updates the js script since its hashed now
+    }),
+  ],
   module: {
     rules: [
       {
