6d76a9bab2cba2b3fc48591f1bb4511388879755
Nicholas Ang <nicholas.ang@data4life-asia.care>
2022-04-14 17:00:20 +0800

    add prod and dev configs, dev-serve

---
+++ webpack.dev.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(common, {
+  mode: "development", // production mode by default
+  devtool: false, // remove eval plus other words so that its easier to read the built code
+  output: {
+    filename: "main.js", //content hashing to cache bust
+    path: path.resolve(__dirname, "dist"), // __dirname in a node script returns the path of the folder where the current JavaScript file resides.
+  },
+});
